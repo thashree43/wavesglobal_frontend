25bfe84ba28dbea45ec7b7edc25fbe7ffe7180e6
Nazeeh Nahaban <nazeehnahaban09@gmail.com>
2025-08-25 21:17:34 +0530

    ui fixed 6

--- src/Routes/AppRoutes.jsx
+++ src/Routes/AppRoutes.jsx
@@ -1,20 +1,19 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ScrollToTop from "../Components/ReusableComponent/Scroll";
 import UserRoutes from "./UserRoutes";
 import AdminRoutes from "./AdminRoutes";
-import ScrollToTop from "../Components/ReusableComponent/Scroll";
-import { Route, HashRouter as Router, Routes } from "react-router-dom";
 
 const AppRoute = () => {
   return (
     <Router>
-      <ScrollToTop /> 
+      <ScrollToTop />
       <Routes>
-        <Route path="/*" element={<UserRoutes />} />
         <Route path="/admin/*" element={<AdminRoutes />} />
+        <Route path="/*" element={<UserRoutes />} />
       </Routes>
     </Router>
   );
 };
-
 
 export default AppRoute;
