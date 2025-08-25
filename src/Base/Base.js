7b9071a1dfe8b48d827ff5ff7aeb134990b0ceeb
Nazeeh Nahaban <nazeehnahaban09@gmail.com>
2025-08-25 21:54:29 +0530

    ui fixed 9

--- src/Base/Base.js
+++ src/Base/Base.js
@@ -1,2 +1,2 @@
-// export const baseurl = 'https://wavesgobal-backend-1.onrender.com/api/'
-export const baseurl = 'http://localhost:3000/api/'
+export const baseurl = 'https://wavesgobal-backend-1.onrender.com/api/'
+// export const baseurl = 'http://localhost:3000/api/'
