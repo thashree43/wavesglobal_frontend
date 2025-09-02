54313c4840a6fb49a1da3e0ed2d12f77f88cdc04
Nazeeh Nahaban <nazeehnahaban09@gmail.com>
2025-09-02 22:51:27 +0530

    before google1

--- src/Base/Base.js
+++ src/Base/Base.js
@@ -1,6 +1,6 @@
-// export const baseurl = 'https://wavesgobal-backend-1.onrender.com/api/'
+export const baseurl = 'https://wavesgobal-backend-1.onrender.com/api/'
 
 
-export const baseurl = `http://localhost:3000/api/`
+// export const baseurl = `http://localhost:3000/api/`
 
 
