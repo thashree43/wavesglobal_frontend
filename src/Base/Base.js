16746a6dc6a749ee32c95409204cfc1283491490
Nazeeh Nahaban <nazeehnahaban09@gmail.com>
2025-09-04 16:17:54 +0530

    before paypal3

--- src/Base/Base.js
+++ src/Base/Base.js
@@ -1,7 +1,7 @@
-// export const baseurl = 'https://wavesgobal-backend-1.onrender.com/api/'
+export const baseurl = 'https://wavesgobal-backend-1.onrender.com/api/'
 export const ClientId = "418114033455-b5m2g67ikop5a7nte7jccg5rsasah0av.apps.googleusercontent.com"
 
-export const baseurl = `http://localhost:3000/api/`
+// export const baseurl = `http://localhost:3000/api/`
 
 
 
