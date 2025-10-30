55ca0d6129b8e519b689940e95fd9ea923931c8d
Nazeeh Nahaban <nazeehnahaban09@gmail.com>
2025-10-30 22:55:24 +0530

    payment gate1

--- src/Base/Base.js
+++ src/Base/Base.js
@@ -1,5 +1,5 @@
 // export const baseurl = 'https://wavesgobal-backend-1.onrender.com/api/'
-const baseurl='https://wavesgobal-backend.onrender.com'
+export const baseurl='https://wavesgobal-backend.onrender.com/api/'
 export const ClientId = "418114033455-b5m2g67ikop5a7nte7jccg5rsasah0av.apps.googleusercontent.com"
 
 // export const baseurl = `http://localhost:3000/api/`
