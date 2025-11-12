17d4a7dc85cb2a85e552762a3c5356d5ecdbd117
Nazeeh Nahaban <nazeehnahaban09@gmail.com>
2025-11-12 12:45:28 +0530

    form changed2

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,33 @@ import { GoogleOAuthProvider } from "@react-oauth/google";
 import AppRoute from "./Routes/AppRoutes";
 import { ClientId } from "./Base/Base";
 import { AuthProvider } from "./Context/Auth";
+import { Toaster } from 'react-hot-toast';
 
 function App() {
   return (
     <>
+      <Toaster 
+        position="top-right"
+        toastOptions={{
+          duration: 4000,
+          style: {
+            background: '#363636',
+            color: '#fff',
+          },
+          success: {
+            iconTheme: {
+              primary: '#10b981',
+              secondary: '#fff',
+            },
+          },
+          error: {
+            iconTheme: {
+              primary: '#ef4444',
+              secondary: '#fff',
+            },
+          },
+        }}
+      />
     <GoogleOAuthProvider clientId={ClientId}>
       <AuthProvider>
         <AppRoute />
